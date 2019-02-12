442d44e04b467803ff47256bee485351b3f409ff
Matt25969 <matt.jo.hunt@gmail.com>
2019-02-12 07:35:07 +0000

    Added more notes and clarification

--- src/main/java/com/qa/persistence/domain/Account.java
+++ src/main/java/com/qa/persistence/domain/Account.java
@@ -2,6 +2,10 @@ package com.qa.persistence.domain;
 
 public class Account {
 	
-	//A comment
-
+	//This class needs to have:
+	//An id
+	//An Account Number
+	//A First Name
+	//A last Name
+	
 }
--- src/main/java/com/qa/persistence/repository/AccountMapRepository.java
+++ src/main/java/com/qa/persistence/repository/AccountMapRepository.java
@@ -11,6 +11,12 @@ public class AccountMapRepository implements AccountRepository{
 	
 	//You must provide concrete implementation for each of these methods
 	//do not change the method signature
+	//THINK - if the parameter is a String, or the return type is a String
+	//How can I convert to a String from an Object?
+	//What utility methods do I have available?
+	
+	//You must complete this section using TDD
+	//You can use the suggested tests or build your own.
 
 	public String getAllAccounts() {
 		// TODO Auto-generated method stub
@@ -22,12 +28,12 @@ public class AccountMapRepository implements AccountRepository{
 		return null;
 	}
 
-	public String deleteAccount(Long id) {
+	public String deleteAccount(int id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public String updateAccount(Long id, String account) {
+	public String updateAccount(int id, String account) {
 		// TODO Auto-generated method stub
 		return null;
 	}
