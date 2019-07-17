73cf51a0ad2096e6bfd78d1b4c52c172a72f16fd
pjesus140 <pierre.jesus@academytrainee.com>
2019-07-17 12:39:44 +0100

    my stuff

--- src/main/java/com/qa/persistence/repository/AccountRepository.java
+++ src/main/java/com/qa/persistence/repository/AccountRepository.java
@@ -1,15 +1,10 @@
 package com.qa.persistence.repository;
 
 public interface AccountRepository {
-	final String SUCCESS = "Operation failed";
-	final String FAILURE = "Operation succeeded";
 
 	String getAllAccounts();
-
-	String createAccount(String account);
-
-	String deleteAccount(int accountNumber);
-
-	String updateAccount(int accountNumber, String account);
+	String createAccount(int accNum, String fName, String lName);
+	String deleteAccount(long id);
+	String updateAccount(long id, int accNum, String fName, String lName);
 
 }
